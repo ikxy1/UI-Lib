aa1531ba59ae92f3ce575f65a13588a97f3a4ec8
ikxy1 <146652351+ikxy1@users.noreply.github.com>
2024-06-25 01:53:13 -0400

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,16 +1,3 @@
--- / Locals
-local Workspace = game:GetService("Workspace")
-local Player = game:GetService("Players").LocalPlayer
-local Mouse = Player:GetMouse()
-
--- / Services
-local UserInputService = game:GetService("UserInputService")
-local TextService = game:GetService("TextService")
-local TweenService = game:GetService("TweenService")
-local RunService = game:GetService("RunService")
-local CoreGuiService = game:GetService("CoreGui")
-local ContentService = game:GetService("ContentProvider")
-local TeleportService = game:GetService("TeleportService")
 --// CUSTOM DRAWING
 
 local drawing = {} do
